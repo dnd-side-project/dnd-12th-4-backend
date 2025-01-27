2eac24d6ad39ed57bf5f77fd1a5a671243967268
hum02 <ehdgma02@gmail.com>
2025-01-27 17:57:09 +0900

    chore: spring 초기설정

---
+++ src/main/java/com/dnd4th/pickitalki/PickitalkiApplication.java
@@ -0,0 +1,13 @@
+package com.dnd4th.pickitalki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickitalkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickitalkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd4th/pickitalki/PickitalkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd4th.pickitalki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickitalkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
