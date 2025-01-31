fe1192349cd1403c1dc4ea87d64cf9bb3c25acba
PlusUltraCode <kkd06144@naver.com>
2025-01-31 10:02:43 +0900

    [feat] : 인터셉터와 resolver 를 WebConfig 에 등록

---
+++ src/main/java/com/dnd12th_4/pickitalki/common/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.dnd12th_4.pickitalki.common.config;
+
+import com.dnd12th_4.pickitalki.common.interceptor.JwtInterceptor;
+import com.dnd12th_4.pickitalki.common.resolver.UserEmailResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtInterceptor jwtInterceptor;
+    private final UserEmailResolver userEmailResolver;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/auth/**","/public/**","/swagger/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userEmailResolver);
+    }
+}
