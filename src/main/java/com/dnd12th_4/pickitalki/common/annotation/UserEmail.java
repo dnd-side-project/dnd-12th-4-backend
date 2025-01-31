419286efb46399e03393b639eef951cb4a7893e1
PlusUltraCode <kkd06144@naver.com>
2025-01-31 09:52:44 +0900

    [feat] : UserEmail 어노테이션 구현

---
+++ src/main/java/com/dnd12th_4/pickitalki/common/annotation/UserEmail.java
@@ -0,0 +1,9 @@
+package com.dnd12th_4.pickitalki.common.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@interface UserEmail {
+}
