69f7d7e38899faace58ee07a6a697bf308ffef31
PlusUltraCode <kkd06144@naver.com>
2025-01-31 10:02:21 +0900

    [feat] : Resolver 를 이용하여 UserMail 손쉽게 가져오기

---
+++ src/main/java/com/dnd12th_4/pickitalki/common/resolver/UserEmailResolver.java
@@ -0,0 +1,32 @@
+package com.dnd12th_4.pickitalki.common.resolver;
+
+import com.dnd12th_4.pickitalki.common.annotation.UserEmail;
+import com.dnd12th_4.pickitalki.common.token.JwtProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class UserEmailResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(UserEmail.class) &&
+                parameter.getParameterType().equals(String.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        return request.getAttribute("useremail");
+    }
+}
