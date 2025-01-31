8ac6eb5277fcef18fd39f32b6d7f0564c6706f77
PlusUltraCode <kkd06144@naver.com>
2025-01-31 12:01:02 +0900

    [Refactor] : 카카오정보에서 이메일이 없는 사용자 임시이메일 생성

--- src/main/java/com/dnd12th_4/pickitalki/service/login/KakaoUserService.java
+++ src/main/java/com/dnd12th_4/pickitalki/service/login/KakaoUserService.java
@@ -26,9 +26,12 @@ public class KakaoUserService {
         Map<String, Object> kakaoAccount = (Map<String, Object>) response.getBody().get("kakao_account");
         Map<String,Object> profile = (Map<String,Object>)kakaoAccount.get("profile");
 
+        String email = kakaoAccount.get("email") != null ? kakaoAccount.get("email").toString()
+                : "no-email-" + response.getBody().get("id").toString() + "@kakao.com";
+
         return KakaoUserDto.builder()
                 .id(response.getBody().get("id").toString())
-                .email(kakaoAccount.get("email").toString())
+                .email(email)
                 .nickname(profile.get("nickname").toString())
                 .profileImageUrl(profile.get("profile_image_url").toString())
                 .build();
