5ec6bdfaf5ad0a634caf28a602fb1abb22ef5736
hum02 <ehdgma02@gmail.com>
2025-02-01 16:11:25 +0900

    feat: 채널 도메인 uuid 사용 위한 Persistable구현

--- src/main/java/com/dnd12th_4/pickitalki/domain/channel/Channel.java
+++ src/main/java/com/dnd12th_4/pickitalki/domain/channel/Channel.java
@@ -5,30 +5,26 @@ import com.dnd12th_4.pickitalki.domain.question.TodayQuestion;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
+import org.springframework.data.domain.Persistable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import static java.util.Objects.isNull;
+
 @Getter
 @Table(name = "channels")
 @Entity
-public class Channel extends BaseEntity {
+public class Channel extends BaseEntity implements Persistable<String> {
 
     @Id
     @Column(columnDefinition = "BINARY(16)")
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
-    @JdbcTypeCode(SqlTypes.BINARY)
     private UUID uuid;
 
     @Column(nullable = false, length = 10)
@@ -47,4 +43,18 @@ public class Channel extends BaseEntity {
         this.name = name;
     }
 
+    public Channel(UUID uuid, String name) {
+        this.uuid = uuid;
+        this.name = name;
+    }
+
+    @Override
+    public boolean isNew() {
+        return isNull(createdAt);
+    }
+
+    @Override
+    public String getId() {
+        return uuid.toString();
+    }
 }
