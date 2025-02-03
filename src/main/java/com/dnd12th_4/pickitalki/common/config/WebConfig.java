51363e2b529311a452e65fe88e61af949d02fe9d
PlusUltraCode <164465431+PlusUltraCode@users.noreply.github.com>
2025-02-03 20:31:22 +0900

    Update WebConfig.java

--- src/main/java/com/dnd12th_4/pickitalki/common/config/WebConfig.java
+++ src/main/java/com/dnd12th_4/pickitalki/common/config/WebConfig.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    private final JwtInterceptor jwtInterceptor;
+
     private final MemberIdResolver memberIdResolver;
 
     //CORS 호완 문제때문에 swagger-ui가 원하는 대로 동작은 안할 수 있다고 해서 일단은 넣었습니다.
