34e623490d98fdc81b8dada5d3f9fea8beeab647
hum02 <ehdgma02@gmail.com>
2025-02-06 14:34:52 +0900

    chore: Question컨트롤러의 dto들 패키지로 이동

--- src/main/java/com/dnd12th_4/pickitalki/controller/question/QuestionResponse.java
+++
@@ -1,4 +0,0 @@
-package com.dnd12th_4.pickitalki.controller.question;
-
-public record QuestionResponse(String authorName, long signalNumber, String content, String createdAt) {
-}
--- src/main/java/com/dnd12th_4/pickitalki/controller/question/dto/QuestionCreateRequest.java
+++ src/main/java/com/dnd12th_4/pickitalki/controller/question/dto/QuestionCreateRequest.java
@@ -1,4 +1,4 @@
-package com.dnd12th_4.pickitalki.controller.question;
+package com.dnd12th_4.pickitalki.controller.question.dto;
 
 public record QuestionCreateRequest(String content, boolean isAnonymous, String anonymousName) {
 }
