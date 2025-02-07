7c969e3456ab33b0e9662e398769e22b5a4c3540
hum02 <ehdgma02@gmail.com>
2025-02-07 14:13:18 +0900

    feat: cors설정

--- src/main/java/com/dnd12th_4/pickitalki/common/config/WebConfig.java
+++ src/main/java/com/dnd12th_4/pickitalki/common/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.dnd12th_4.pickitalki.common.config;
 import com.dnd12th_4.pickitalki.common.interceptor.JwtInterceptor;
 import com.dnd12th_4.pickitalki.common.resolver.MemberIdResolver;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -20,18 +19,14 @@ public class WebConfig implements WebMvcConfigurer {
     private final MemberIdResolver memberIdResolver;
 
     //CORS 호완 문제때문에 swagger-ui가 원하는 대로 동작은 안할 수 있다고 해서 일단은 넣었습니다.
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Refresh-Token")
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 
     @Override
