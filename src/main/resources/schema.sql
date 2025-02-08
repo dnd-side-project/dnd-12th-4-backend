9be093f757ea9e6c5792f23cb1d24b9e171f76df
hum02 <ehdgma02@gmail.com>
2025-02-08 14:16:18 +0900

    feat: 채널의 이름을 중복 불가능하게 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS members
+create TABLE if not exists `pickitalki`.members
 (
     id                BIGINT AUTO_INCREMENT PRIMARY KEY,
     kakao_id          BIGINT UNIQUE NOT NULL,
@@ -7,20 +7,19 @@ CREATE TABLE IF NOT EXISTS members
     profile_image_url TEXT          NULL,
     refresh_token     TEXT          NULL,
     created_at        DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at        DATETIME               DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at        DATETIME               DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP,
     is_deleted        TINYINT(1)    NOT NULL DEFAULT 0
 );
 
-CREATE TABLE IF NOT EXISTS channels
+create TABLE if not exists `pickitalki`.channels
 (
     uuid       BINARY(16) PRIMARY KEY,
-    name       VARCHAR(30) NOT NULL,
+    name       VARCHAR(30) NOT NULL UNIQUE,
     created_at DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME             DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at DATETIME             DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP,
     is_deleted TINYINT(1)  NOT NULL DEFAULT 0
 );
-
-CREATE TABLE IF NOT EXISTS channel_members
+create TABLE if not exists `pickitalki`.channel_members
 (
     id               BIGINT AUTO_INCREMENT PRIMARY KEY,
     channel_uuid     BINARY(16)  NOT NULL,
@@ -28,43 +27,41 @@ CREATE TABLE IF NOT EXISTS channel_members
     member_code_name VARCHAR(20),
     role             VARCHAR(10) NOT NULL,
     created_at       DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at       DATETIME             DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at       DATETIME             DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP,
     is_deleted       TINYINT(1)  NOT NULL DEFAULT 0,
     FOREIGN KEY (channel_uuid) REFERENCES channels (uuid),
     FOREIGN KEY (member_id) REFERENCES members (id)
 );
 
-CREATE TABLE IF NOT EXISTS questions
+create TABLE if not exists `pickitalki`.questions
 (
-    id                BIGINT AUTO_INCREMENT PRIMARY KEY,
-    channel_uuid      BINARY(16)   NOT NULL,
-    channel_member_id BIGINT       NOT NULL,
-    content           VARCHAR(100) NOT NULL,
-    question_number   BIGINT       NOT NULL,
-    is_anonymous      BOOLEAN      NOT NULL DEFAULT FALSE,
-    author_name       VARCHAR(10),
-    created_date      DATE         NOT NULL,
-    created_at        DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at        DATETIME              DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    is_deleted        TINYINT(1)   NOT NULL DEFAULT 0,
+    id             BIGINT AUTO_INCREMENT PRIMARY KEY,
+    channel_uuid   BINARY(16)   NOT NULL,
+    author_id      BIGINT       NOT NULL,
+    content        VARCHAR(255) NOT NULL,
+    is_anonymous   BOOLEAN      NOT NULL DEFAULT FALSE,
+    anonymous_name VARCHAR(30),
+    created_at     DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at     DATETIME              DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP,
+    is_deleted     TINYINT(1)   NOT NULL DEFAULT 0,
+    CONSTRAINT unique_channel_today_question UNIQUE (channel_uuid, created_at),
     FOREIGN KEY (channel_uuid) REFERENCES channels (uuid),
-    FOREIGN KEY (channel_member_id) REFERENCES channel_members (id),
-    CONSTRAINT unique_channel_today_question UNIQUE (channel_uuid, created_date)
+    FOREIGN KEY (author_id) REFERENCES members (id)
 );
 
-CREATE TABLE IF NOT EXISTS answers
+create TABLE if not exists `pickitalki`.answers
 (
-    id                BIGINT AUTO_INCREMENT PRIMARY KEY,
-    question_id       BIGINT       NOT NULL,
-    channel_member_id BIGINT       NOT NULL,
-    content           VARCHAR(500) NOT NULL,
-    is_anonymous      BOOLEAN      NOT NULL DEFAULT FALSE,
-    anonymous_name    VARCHAR(30),
-    created_at        DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at        DATETIME              DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    is_deleted        TINYINT(1)   NOT NULL DEFAULT 0,
+    id             BIGINT AUTO_INCREMENT PRIMARY KEY,
+    question_id    BIGINT       NOT NULL,
+    member_id      BIGINT       NOT NULL,
+    content        VARCHAR(500) NOT NULL,
+    is_anonymous   BOOLEAN      NOT NULL DEFAULT FALSE,
+    anonymous_name VARCHAR(10),
+    created_at     DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at     DATETIME              DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP,
+    is_deleted     TINYINT(1)   NOT NULL DEFAULT 0,
     FOREIGN KEY (question_id) REFERENCES questions (id),
-    FOREIGN KEY (channel_member_id) REFERENCES channel_members (id)
+    FOREIGN KEY (member_id) REFERENCES members (id)
 );
 
 CREATE TABLE IF NOT EXISTS tutorial
@@ -74,6 +71,5 @@ CREATE TABLE IF NOT EXISTS tutorial
     status     VARCHAR(10) NOT NULL,
     created_at DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME             DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    is_deleted TINYINT(1)  NOT NULL DEFAULT 0,
-    FOREIGN KEY (member_id) REFERENCES members (id)
+    is_deleted TINYINT(1)  NOT NULL DEFAULT 0
 );
