ae160c43b06091d1137716d1489db9e8f07423d9
hum02 <ehdgma02@gmail.com>
2025-02-11 15:00:04 +0900

    feat: 이미지 조회에 대한 인터셉터 우회

--- src/main/java/com/dnd12th_4/pickitalki/common/config/WebConfig.java
+++ src/main/java/com/dnd12th_4/pickitalki/common/config/WebConfig.java
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/auth/**","/public/**","/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html","/hello/**");
+                .excludePathPatterns("/images/**", "/auth/**","/public/**","/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html","/hello/**");
     }
 
     @Override
