4aece8c293ed81d1332bacaa0c5f7132c4822943
hum02 <ehdgma02@gmail.com>
2025-02-17 13:59:08 +0900

    fix: image url 매핑 오류 해결

--- src/main/java/com/dnd12th_4/pickitalki/common/config/AppConfig.java
+++ src/main/java/com/dnd12th_4/pickitalki/common/config/AppConfig.java
@@ -1,15 +1,17 @@
 package com.dnd12th_4.pickitalki.common.config;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@Getter
 public class AppConfig {
 
     @Value("${app.base-url}")
-    private static String baseUrl;
+    private String baseUrl;
 
     @Bean
     public RestTemplate restTemplate(){
